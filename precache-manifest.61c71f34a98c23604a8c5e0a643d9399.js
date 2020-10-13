08aef02778a9f78ef7b2b4f7da442dc0281c9899
Krzysztofa Kołodziejczak <krzysztofa.kolodziejczak@gmail.com>
2020-10-13 17:08:16 +0200

    Updates

---
+++ precache-manifest.61c71f34a98c23604a8c5e0a643d9399.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c8cc2b89f76272bf07b1edceba6119",
+    "url": "/git@github.com:KrzysKolo/appGoogleBooksSearch.git/index.html"
+  },
+  {
+    "revision": "4698723904a188f88891",
+    "url": "/git@github.com:KrzysKolo/appGoogleBooksSearch.git/static/css/main.7344657f.chunk.css"
+  },
+  {
+    "revision": "566296c7aedc2fca1373",
+    "url": "/git@github.com:KrzysKolo/appGoogleBooksSearch.git/static/js/2.c97f7b1c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/git@github.com:KrzysKolo/appGoogleBooksSearch.git/static/js/2.c97f7b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4698723904a188f88891",
+    "url": "/git@github.com:KrzysKolo/appGoogleBooksSearch.git/static/js/main.a7e41b74.chunk.js"
+  },
+  {
+    "revision": "bb88e31cbade69be0b2a",
+    "url": "/git@github.com:KrzysKolo/appGoogleBooksSearch.git/static/js/runtime-main.c7ebb51f.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/git@github.com:KrzysKolo/appGoogleBooksSearch.git/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "48650bfe0ac6b3712525b0642281035e",
+    "url": "/git@github.com:KrzysKolo/appGoogleBooksSearch.git/static/media/logo.48650bfe.png"
+  }
+]);
